82121207ee8fd97c236ca7f310157b526d441f8b
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-03-12 17:07:03 +0100

    es

---
+++ Untitled.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE db_university;
+USE db_university;
+
+/*Selezionare tutti gli studenti nati nel 1990 (160)*/
+SELECT *
+FROM students
+WHERE YEAR(date_of_birth) = 1990;
+
+/*Selezionare tutti i corsi che valgono più di 10 crediti (479)*/
+SELECT *
+FROM courses
+WHERE cfu > 10;
+
+/*Selezionare tutti gli studenti che hanno più di 30 anni*/
+SELECT *
+FROM students
+WHERE timestampdiff(YEAR, `date_of_birth`, CURDATE()) > 30;
+
+/*Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)*/
+SELECT *
+FROM courses
+WHERE period = 'I semestre'
+AND YEAR = 1;
+
+/*Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)*/
+SELECT *
+FROM exams
+WHERE date = '2020-06-20'
+AND HOUR(hour) >= 14;
+
+/*Selezionare tutti i corsi di laurea magistrale (38)*/
+SELECT *
+FROM degrees
+WHERE level = 'magistrale';
+
+/*Da quanti dipartimenti è composta l'università?*/
+SELECT *
+FROM departments;
+
+/*Quanti sono gli insegnanti che non hanno un numero di telefono?*/
+SELECT *
+FROM teachers
+WHERE phone IS NULL;
